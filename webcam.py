dd84cab98201305154fdf67c3993d91a9e4c276e
SrishtiYadav094 <srishtiyadav1245@gmail.com>
2021-03-01 13:51:12 +0530

    cam

---
+++ webcam.py
@@ -0,0 +1,11 @@
+import cv2
+webcam=cv2.VideoCapture(0)
+while True:
+    state,img=webcam.read()
+    if state:
+        cv2.imshow("mycam",img)
+        if cv2.waitKey(1)==ord('q'):
+            break
+#out of while
+webcam.release()
+cv2.destroyAllWindows()
